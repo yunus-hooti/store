d654c259b4cb2cb521ee2c22291b5f6e733bdc1f
yons hooti <younes.hooti2003@gmail.com>
2026-06-11 13:29:52 +0330

    add emil password rest

--- account/urls.py
+++ account/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
     path('password-reset/', auth_views.PasswordResetView.as_view(template_name="registration/password_reset_form.html",
                                                                  success_url=reverse_lazy('account:password_reset_done'),
                                                                  # استفاده از نام مسیر
-                                                                 email_template_name='registration/password_reset_email.html'),name='password_reset'),
+                                                                 html_email_template_name='registration/password_reset_email.html'),name='password_reset'),
     path('password-reset/done/',
          auth_views.PasswordResetDoneView.as_view(template_name="registration/password_reset_done.html"),
          name='password_reset_done'),
--- store/settings.py
+++ store/settings.py
@@ -131,4 +131,9 @@ LOGOUT_URL = 'account:logout'
 LOGOUT_REDIRECT_URL = "account:login"
 LOGIN_REDIRECT_URL = "account:profile"
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'mostafa.hooti789@gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_PASSWORD = 'fbbkdyakttcextco'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
