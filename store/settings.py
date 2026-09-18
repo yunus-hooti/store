ed28cd5b4f6172a638c1b4ca7aa036af882df3a5
yons hooti <younes.hooti2003@gmail.com>
2026-09-18 17:53:57 +0330

    Changing the database to PostgreSQL

--- store/settings.py
+++ store/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'store.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'store',
+        'USER': 'yons_admin',
+        'PASSWORD': 'qazx50501',
+        'PORT':5432,
+        'HOST':'localhost',
     }
 }
 
